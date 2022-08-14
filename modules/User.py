d1d240431b5f8265057b5dd4702a9ed11694f348
Pers2140 <Pers2140@fredonia.edu>
2022-08-14 01:07:06 +0000

    correcting branch

--- modules/User.py
+++ modules/User.py
@@ -5,11 +5,38 @@ import sqlite3
 
 
 # create sql db instance
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 # create SQL Model
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+class User():
+    '''
+        User class for signing up
+    '''
+    
+    def __init__(self,user):
+        
+        self.first_name = user['first_name'] 
+        self.last_name = user['last_name']
+        self.username= user['username']
+        self.password = user['password']
+        self.email = user['email']
+    
+        # On user creation submit information to DB
+        # Connect to database
+        db_conn = sqlite3.connect('test.db') 
+        
+        # Cursor to interact with DB
+        c = db_conn.cursor()
+        
+        # submit pano 
+        c.execute("INSERT INTO users ('username','password','email') VALUES (?,?,?)", ( self.username, self.username, self.email))
+        print('New user submitted to DB new ')
+        
+        # close connection to DB
+        db_conn.commit()
+        db_conn.close()
+    
  
+
+    def __repr__(self):
+        return '<Name %r> % self.name'
