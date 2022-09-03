0c9f5a0d4fce1e95da0d5cfb5cab7f850de76c99
Pers2140 <Pers2140@fredonia.edu>
2022-09-03 00:51:51 +0000

    trying to add db to heroku

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, url_for, redirect, request
-from flask_login import login_user, LoginManager, login_required, logout_user, current_user
-from flask_cors import CORS, cross_origin   
-from flask_bcrypt import Bcrypt
-import json
 
-from modules.form import *
-from modules.favpano import *
-from modules.User import *
+from flask import Flask, render_template, url_for, redirect, request
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
+# from modules.form import *
+# from modules.favpano import *
+# from modules.User import *
 
 """
     To have live server update on change set FLASK vars
@@ -17,8 +19,6 @@ from modules.User import *
 
 # create instance
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 # add database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nczkgddnkjvutl:f40edd2c47cbc0ac444e4161c157b6f5fcdc56176a510f45d2bbae798f0bb16e@ec2-34-234-240-121.compute-1.amazonaws.com:5432/d7kp6qcmapcc7u'
 app.config["SECRET_KEY"] = "f40edd2c47cbc0ac444e4161c157b6f5fcdc56176a510f45d2bbae798f0bb16e"
@@ -29,21 +29,34 @@ login_manager.login_view = 'login'
 # add bcrypt to app
 bcrypt = Bcrypt(app)
 # create sql db instance
-# db.init_app(app)         
-
-db = SQLAlchemy()
-
-
-# create sql db instance
-# db = SQLAlchemy()
+db = SQLAlchemy(app)
 
 # create SQL Model
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    panos = db.Column(db.String(), nullable=False)
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80))
+    
+# create Signup Form
+class SignUpForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    email = StringField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Email"})
+    submit = SubmitField('Register')
+    
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+            
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+    
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
