aa2fee57d2dce7732543fb9c3f669b42e1b34ac8
Pers2140 <Pers2140@fredonia.edu>
2022-09-06 20:17:27 +0000

    reverted to original app

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ from flask import Flask, render_template, url_for, redirect, request
 from flask_login import login_user, LoginManager, login_required, logout_user, current_user
 from flask_cors import CORS, cross_origin   
 from flask_bcrypt import Bcrypt
-from sqlalchemy import true
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 import json
 
 from modules.form import *
-# from modules.favpano import *
-# from modules.User import *
+from modules.favpano import *
+from modules.User import *
 
 """
     To have live server update on change set FLASK vars
@@ -17,37 +14,22 @@ from modules.form import *
     export FLASK_APP=app.py
     flask run
 """
-db = SQLAlchemy()
 
-
-# create sql db instance
-# db = SQLAlchemy()
-
-# create SQL Model
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    panos = db.Column(db.String(), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80))
 # create instance
 app = Flask(__name__)
-# create sql db instance
-db.init_app(app)         
-with app.app_context():
-    db.create_all()
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 # add database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gbkwzleyaglirn:078915c822e17cbb4c2cb86fe7d8ff000b9f15cc5d3a7fbe2049bbb7810b057b@ec2-34-234-240-121.compute-1.amazonaws.com:5432/d38mm771ie34mq'
-app.config["SECRET_KEY"] = "078915c822e17cbb4c2cb86fe7d8ff000b9f15cc5d3a7fbe2049bbb7810b057b"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SECRET_KEY"] = "password"
 # load flask_login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 # add bcrypt to app
 bcrypt = Bcrypt(app)
-
+# create sql db instance
+db.init_app(app)         
 
     
 @login_manager.user_loader
