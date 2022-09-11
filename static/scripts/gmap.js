5dfcfe323924b02e53c7f3fbfe1ea2d51ec06ee7
Pers2140 <Pers2140@fredonia.edu>
2022-09-11 02:36:41 +0000

    added panosnap.herokuapp.com

--- static/scripts/gmap.js
+++ static/scripts/gmap.js
@@ -26,7 +26,7 @@ function myMap() {
         // send fav pano details
         let data = panorama.location
         
-        fetch("http://127.0.0.1/fav", {
+        fetch("http://panosnap.herokuapp.com/fav", {
           method: "POST",
           headers: {'Content-Type': 'application/json'}, 
           body: JSON.stringify(data)
