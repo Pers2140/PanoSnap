f15713f8635f5d3f9db220ecbd88d83c2146a7a6
Pers2140 <Pers2140@fredonia.edu>
2022-09-11 02:28:27 +0000

    changed to http

--- static/scripts/gmap.js
+++ static/scripts/gmap.js
@@ -26,7 +26,7 @@ function myMap() {
         // send fav pano details
         let data = panorama.location
         
-        fetch("https://127.0.0.1:5000/fav", {
+        fetch("http://127.0.0.1:5000/fav", {
           method: "POST",
           headers: {'Content-Type': 'application/json'}, 
           body: JSON.stringify(data)
